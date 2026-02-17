ef5b3719a6e98fae15f79f4fdaa017ceaa9a3952
camilaraissarodrigues@gmail.com <camilaraissarodrigues@gmail.com>
2026-02-17 19:59:33 +0000

    alguns ajustes nos testes e na base ativa, mas ela ainda não está correta

--- models/active_base.sql
+++ models/active_base.sql
@@ -1,46 +1,47 @@
 WITH monthly_calendar AS (
 
-    -- gera meses entre primeira e última venda
     SELECT month
     FROM UNNEST(
         GENERATE_DATE_ARRAY(
-            (SELECT DATE_TRUNC(MIN(purchase_date), MONTH) FROM {{ref("sales_ecom")}}),
-            (SELECT DATE_TRUNC(MAX(purchase_date), MONTH) FROM {{ref("sales_ecom")}}),
+            DATE_TRUNC((SELECT MIN(purchase_date) FROM {{ ref('sales_ecom') }}), MONTH),
+            DATE_TRUNC((SELECT MAX(purchase_date) FROM {{ ref('sales_ecom') }}), MONTH),
             INTERVAL 1 MONTH
         )
     ) AS month
 
 ),
 
-last_purchase AS (
+last_purchase_per_client AS (
 
     SELECT
         client_id,
         MAX(purchase_date) AS last_purchase_date
-    FROM {{ref("sales_ecom")}}
+    FROM {{ ref('sales_ecom') }}
     GROUP BY client_id
 
 ),
 
-customer_status AS (
-
-    SELECT
-        client_id,
-        last_purchase_date,
-        DATE_ADD(last_purchase_date, INTERVAL 90 DAY) AS churn_date
-    FROM last_purchase
-
-),
-
+-- Base ativa = clientes com compra nos últimos 90 dias antes do mês
 active_base AS (
 
     SELECT
         m.month,
         COUNT(DISTINCT s.client_id) AS active_base
     FROM monthly_calendar m
-    JOIN {{ref("sales_ecom")}} s
-        ON s.purchase_date < m.month
+    JOIN {{ ref('sales_ecom') }} s
+        ON s.purchase_date BETWEEN DATE_SUB(m.month, INTERVAL 90 DAY)
+                               AND DATE_SUB(m.month, INTERVAL 1 DAY)
     GROUP BY m.month
+
+),
+
+-- churn_date = data em que completa 90 dias sem compra
+customer_churn_dates AS (
+
+    SELECT
+        client_id,
+        DATE_ADD(last_purchase_date, INTERVAL 90 DAY) AS churn_date
+    FROM last_purchase_per_client
 
 ),
 
@@ -49,8 +50,7 @@ monthly_churn AS (
     SELECT
         DATE_TRUNC(churn_date, MONTH) AS month,
         COUNT(client_id) AS churned_customers
-    FROM customer_status
-    WHERE churn_date <= (SELECT MAX(purchase_date) FROM {{ref("sales_ecom")}})
+    FROM customer_churn_dates
     GROUP BY month
 
 )
